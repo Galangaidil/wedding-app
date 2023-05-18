7acdee72c5dae7f9cf2e513770d5f7f29abb9903
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-18 13:35:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/weddingapp/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.weddingapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.weddingapp.ui.theme.WeddingAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainContent()
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainContent() {
+    Scaffold(
+        content = { MyContent() }
+    )
+}
+
+@Composable
+fun MyContent() {
+
+    // Declare a string that contains a url
+    val mUrl = "http://192.168.100.17:8000/login"
+
+    // Adding a WebView inside AndroidView
+    // with layout as full screen
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            loadUrl(mUrl)
+        }
+    }, update = {
+        it.loadUrl(mUrl)
+    })
+}
+
+// For displaying preview in
+// the Android Studio IDE emulator
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainContent()
+}
